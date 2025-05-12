8b16aeb8edcbc22650d54cf27251ac0ef6cce11d
Jeovani Stars <jeovanigamati@gmail.com>
2025-05-12 10:01:17 -0300

    Criar abreConta
    Funcionalidade de abrir a conta adicionada

--- src/br/com/ifba/atividade08/java/ContaBanco.java
+++ src/br/com/ifba/atividade08/java/ContaBanco.java
@@ -9,5 +9,62 @@ package br.com.ifba.atividade08.java;
  * @author Bruno
  */
 public class ContaBanco {
+    protected String tipoConta;
+    public int numConta;
+    private String donoConta;
+    private int saldoConta;
+    private boolean statusConta;
+
+    public String getTipoConta() {
+        return tipoConta;
+    }
+
+    public void setTipoConta(String tipoConta) {
+        this.tipoConta = tipoConta;
+    }
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getDonoConta() {
+        return donoConta;
+    }
+
+    public void setDonoConta(String donoConta) {
+        this.donoConta = donoConta;
+    }
+
+    public int getSaldoConta() {
+        return saldoConta;
+    }
+
+    public void setSaldoConta(int saldoConta) {
+        this.saldoConta = saldoConta;
+    }
+
+    public boolean isStatusConta() {
+        return statusConta;
+    }
+
+    public void setStatusConta(boolean statusConta) {
+        this.statusConta = statusConta;
+    }
+
+    public void abrirConta(String tipoConta){
+        this.setTipoConta(tipoConta);
+        this.setStatusConta(true);
+        
+        if(tipoConta.equals("cc")){
+            this.setSaldoConta
+        }
+        
+    }
+    
+    
     
 }
