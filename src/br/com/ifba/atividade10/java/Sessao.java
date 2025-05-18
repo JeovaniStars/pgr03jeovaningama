3baa192684546cf2ff5f4a7c982444c834bef418
Jeovani Stars <jeovanigamati@gmail.com>
2025-05-18 11:18:40 -0300

    Criar funcionalidade Sessao

--- src/br/com/ifba/atividade10/java/Sessao.java
+++ src/br/com/ifba/atividade10/java/Sessao.java
@@ -3,11 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package br.com.ifba.atividade10.java;
-
+import java.time.LocalDateTime;
+import java.util.UUID;
 /**
  *
  * @author Bruno
  */
 public class Sessao {
+    private String idSessao;
+    private Usuario usuario;
+    private LocalDateTime dataCriacao;
+    
+    public Sessao(Usuario usuario) {
+        this.usuario = usuario;
+        this.idSessao = UUID.randomUUID().toString();  // gera id único
+        this.dataCriacao = LocalDateTime.now();
+    }
+
+    public String getIdSessao() {
+        return idSessao;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+    
     
 }
