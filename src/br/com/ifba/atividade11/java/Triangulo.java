a6550cfe1ccbf3f0e0055af4310073d8c18aee40
Jeovani Stars <jeovanigamati@gmail.com>
2025-05-20 13:34:55 -0300

    Adicionar Triangulo

--- src/br/com/ifba/atividade11/java/Triangulo.java
+++ src/br/com/ifba/atividade11/java/Triangulo.java
@@ -9,5 +9,26 @@ package br.com.ifba.atividade11.java;
  * @author Bruno
  */
 public class Triangulo extends Forma2d {
+   
+    private double lado;
+    private double altura;
+    
+    public Triangulo(double lado, double altura) {
+        super("Triangulo");
+        this.lado = lado;
+        this.altura = altura;
+    }
+    
+    @Override
+    public double obterArea(){
+        return (lado * altura) / 2;
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + ", lado = " + lado +
+                ", altura = " + altura +
+                ", área = " + obterArea();
+    }
     
 }
