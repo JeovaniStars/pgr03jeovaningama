b1feb5c417741f111884d3e6848258403c5d7dc9
Jeovani Stars <jeovanigamati@gmail.com>
2025-05-20 13:38:41 -0300

    Adicionado Circulo

--- src/br/com/ifba/atividade11/java/Circulo.java
+++ src/br/com/ifba/atividade11/java/Circulo.java
@@ -9,5 +9,21 @@ package br.com.ifba.atividade11.java;
  * @author Bruno
  */
 public class Circulo extends Forma2d {
+    private double raio;
     
+    public Circulo(double raio) {
+        super("Circulo");
+        this.raio = raio;
+    }
+    
+    @Override
+    public double obterArea(){
+        return Math.PI * Math.pow(raio, 2);
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + ", raio = " + raio +
+               ", área = " + obterArea();
+    }
 }
